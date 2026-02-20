c30182f7a51c404058c7fd40f7a0b8a2612e63a6
onish-cmd <you@example.com>
2026-02-20 11:54:51 +0530

    Fixed package name in kernel.rs and added semicolon

--- src/kernel.rs
+++ src/kernel.rs
@@ -5,22 +5,21 @@
 extern crate limine;
 use limine::request::FramebufferRequest;
 use core::arch::asm;
-use framebuffer;
-
+use vibe-framebuffer;
 static FRAMEBUFFER_REQUEST: FramebufferRequest = FramebufferRequest::new();
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     if let Some(fb_response) = FRAMEBUFFER_REQUEST.get_response() {
         if let Some(fb) = fb_response.framebuffers().next() {
-            let mut cursor = framebuffer::Cursor::new(
+            let mut cursor = vibe-framebuffer::Cursor::new(
                 fb.addr() as *mut u32, 
                 fb.width(), 
                 fb.height()
             );
         }
     }
-    cursor.clear(0x001A1B26)
+    cursor.clear(0x001A1B26);
     loop { unsafe {
             asm!(
                 "hlt"
