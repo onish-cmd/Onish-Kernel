5394e9c4260c9d67ece272b56d4c6ea9f53ae53b
onish-cmd <you@example.com>
2026-02-20 13:49:37 +0530

    Added print

--- drivers/framebuffer/src/lib.rs
+++ drivers/framebuffer/src/lib.rs
@@ -1,13 +1,58 @@
 // drivers/framebuffer/lib.rs
 #![no_std]
 
+#[repr(C)]
+pub struct Psf2Header {
+    pub magic: [u8; 4],
+    pub version: u32,
+    pub header_size: u32,
+    pub flags: u32,
+    pub length: u32,        // Number of glyphs
+    pub char_size: u32,     // Bytes per glyph
+    pub height: u32,        // Height in pixels
+    pub width: u32,         // Width in pixels
+}
+
+const PSF2_MAGIC: [u8; 4] = [0x72, 0xb5, 0x4a, 0x86];
+
+pub struct Font<'a> {
+    pub header: &'a Psf2Header,
+    pub glyphs: &'a [u8],
+}
+
+impl<'a> Font<'a> {
+    pub fn new(data: &'a [u8]) -> Self {
+        let header = unsafe { &*(data.as_ptr() as *const Psf2Header) };
+        
+        // Safety check: verify the magic number
+        if header.magic != PSF2_MAGIC {
+            panic!("Invalid PSF2 font!");
+        }
+
+        // The glyphs start after the header
+        let glyphs_start = header.header_size as usize;
+        let glyphs = &data[glyphs_start..];
+
+        Self { header, glyphs }
+    }
+
+    pub fn get_glyph(&self, c: char) -> &[u8] {
+        let index = c as usize;
+        let start = index * self.header.char_size as usize;
+        let end = start + self.header.char_size as usize;
+        &self.glyphs[start..end]
+    }
+}
+
 pub struct Cursor {
     pub x: usize,
     pub y: usize,
     pub color: u32,
+    pub color_fg: u32,
     pub fb_ptr: *mut u32,
     pub width: u64,
     pub height: u64,
+    pub font: Option<Font<'static>>,
 }
 
 impl Cursor {
@@ -16,6 +61,7 @@ impl Cursor {
             x: 0,
             y: 0,
             color: 0xFFFFFFFF, // Default White
+            color_fg: 0xFF000000, // Default black
             fb_ptr: ptr,
             width,
             height,
@@ -37,6 +83,50 @@ impl Cursor {
         let total_pixels = (self.width * self.height) as usize;
         for i in 0..total_pixels {
             *self.fb_ptr.add(i) = self.color;
+        }
+    }
+
+    // Draws a single character
+    pub fn draw_char(&mut self, c: char) {
+        let font = self.font;
+        let glyph = font.get_glyph(c);
+        let bytes_per_row = (font.header.width + 7) / 8;
+        let f_width = font.header.width as usize;
+        let f_height = font.header.height as usize;
+
+        if self.x + f_width > self.width as usize {
+            self.x = 0;
+            self.y += f_height;
+        }
+
+        if self.y + f_height > self.height as usize {
+            self.y = 0;
+            self.x = 0;
+            clear_screen(self.color)
+        }
+
+        if c == '\n' {
+            self.x = 0;
+            self.y += f_height;
+            return;
+        }
+        for py in 0..font.header.height {
+            for px in 0..font.header.width {
+                let byte_offset = (py * bytes_per_row + px / 8) as usize;
+                let bit_offset = 7 - (px % 8);
+                let bit_is_set = (glyph[byte_offset] >> bit_offset) & 1;
+
+                if bit_is_set == 1 {
+                    unsafe {
+                        self.write_pixel(
+                            self.x + px as usize, 
+                            self.y + py as usize, 
+                            self.color_fg
+                        )
+                    }
+                }
+            }
+            self.x += font.header.width as usize;
         }
     }
 }
--- src/kernel.rs
+++ src/kernel.rs
@@ -2,25 +2,60 @@
 #![no_std]
 #![no_main]
 
+#[macro_export]
+macro_rules! print {
+    ($($arg:tt)*) => ($crate::_print(format_args!($($arg)*)));
+}
+
+#[macro_export]
+macro_rules! println {
+    () => ($crate::print!("\n"));
+    ($($arg:tt)*) => ($crate::print!("{}\n", format_args!($($arg)*)));
+}
+
 extern crate limine;
 use limine::request::FramebufferRequest;
 use core::arch::asm;
 use vibe_framebuffer::Cursor;
+use spleen_font::FONT_16X32;
+use core::fmt::{self, Write};
+use core::fmt;
+
+impl fmt::Write for Cursor {
+    fn write_str(&mut self, s: &str) -> fmt::Result {
+        for c in s.chars() {
+            self.draw_char(c);
+        }
+        Ok(())
+    }
+}
 
 static FRAMEBUFFER_REQUEST: FramebufferRequest = FramebufferRequest::new();
 static mut UI_CURSOR: Option<Cursor> = None;
+
+pub fn _print(args: fmt::Arguments) {
+    unsafe {
+        if let Some(ref mut cursor) = UI_CURSOR {
+            cursor.write_fmt(args).unwrap();
+        }
+    }
+}
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     unsafe { if let Some(fb_response) = FRAMEBUFFER_REQUEST.get_response() {
         if let Some(fb) = fb_response.framebuffers().next() {
+            let font = vibe_framebuffer::Font::new(FONT_16X32);
             UI_CURSOR = Some(Cursor::new(
                 fb.addr() as *mut u32, 
                 fb.width(), 
                 fb.height()
-            )); }
+            ));
+            cursor.font = Some(font);
+        }
         }
     }
+    println!("Vibe OS is alive!");
     clear_screen(0x001A1B26);
     loop { unsafe {
             asm!(
@@ -40,6 +75,7 @@ pub fn clear_screen(color: u32) {
 
 #[panic_handler]
 fn panic(_info: &core::panic::PanicInfo) -> ! { 
+    println!("KERNEL PANIC: {}", info);
     loop { 
         unsafe { 
             asm!("hlt")
