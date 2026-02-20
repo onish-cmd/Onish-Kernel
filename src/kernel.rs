a45d6a346f29e80e34def98af3b64f4b22670fda
onish-cmd <you@example.com>
2026-02-20 11:57:41 +0530

    Fixed package name

--- src/kernel.rs
+++ src/kernel.rs
@@ -5,14 +5,14 @@
 extern crate limine;
 use limine::request::FramebufferRequest;
 use core::arch::asm;
-use vibe-framebuffer;
+use vibe_framebuffer;
 static FRAMEBUFFER_REQUEST: FramebufferRequest = FramebufferRequest::new();
 
 #[no_mangle]
 pub extern "C" fn _start() -> ! {
     if let Some(fb_response) = FRAMEBUFFER_REQUEST.get_response() {
         if let Some(fb) = fb_response.framebuffers().next() {
-            let mut cursor = vibe-framebuffer::Cursor::new(
+            let mut cursor = vibe_framebuffer::Cursor::new(
                 fb.addr() as *mut u32, 
                 fb.width(), 
                 fb.height()
